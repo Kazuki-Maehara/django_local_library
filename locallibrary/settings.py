22e7d7cce29dd1f1e160c741b0a833ab6a6a1fd9
Kazuki Maehara <treize.citron13@gmail.com>
2022-04-09 15:18:07 +0900

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['evening-everglades-93458.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
